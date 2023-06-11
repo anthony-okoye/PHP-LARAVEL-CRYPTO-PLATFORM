ff9ab81de847c6422365d5de1c91263f00c26923
Tee <34031023+anthony-okoye@users.noreply.github.com>
2023-06-11 23:40:41 +0100

    Update register.blade.php
    added Pangea user_intel for better security, I also enforced more robust password patterns if a data breach is found

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <section class="auth-form">
-
+        <div id="message" class="message"></div>
         <div class="container">
             <div class="row">
                 <div class="col-md-8 col-md-offset-2">
@@ -103,3 +103,96 @@
 @if(get_option('enable_recaptcha_registration') == 1)
     <script src='https://www.google.com/recaptcha/api.js'></script>
 @endif
+<script type="text/javascript">
+    document.addEventListener('DOMContentLoaded', function() {
+    const passwordInputs = document.querySelectorAll('input[type="password"]');
+passwordInputs.forEach((input) => {
+  input.setAttribute('readonly', 'readonly');
+});
+
+// Get the email input field
+const emailInput = document.getElementById('email');
+
+// Get the message element
+const messageElement = document.getElementById('message');
+
+// Debounce function to delay API request
+let debounceTimeout;
+
+// Function to handle email input change
+function handleEmailChange() {
+  clearTimeout(debounceTimeout);
+
+  // Delay the API request by 500 milliseconds after the user stops typing
+  debounceTimeout = setTimeout(() => {
+    const email = emailInput.value;
+
+    // API request
+    fetch(`https://user-intel.{{env('PANGEA_DOMAIN')}}/v1/user/breached`, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer {{env('USER_INTEL_AUTH_TOKEN')}}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        provider: 'spycloud',
+        email: email,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Check the response data
+        if (data.result.data.found_in_breach) {
+          // Password found in breach, enable password inputs and enforce stricter password requirements
+          passwordInputs.forEach((input) => {
+            input.removeAttribute('readonly');
+            input.setAttribute(
+              'pattern',
+              '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$'
+            );
+            input.setAttribute(
+              'title',
+              'Password must contain at least 8 characters including at least one uppercase letter, one lowercase letter, one number, and one symbol.'
+            );
+          });
+
+          // Show breach found message in the message element
+          messageElement.textContent = `${data.result.data.breach_count} Data breach found! use a strong and unique password`;
+          messageElement.style.display = 'block';
+          messageElement.style.position = 'fixed';
+          messageElement.style.top = '10px';
+          messageElement.style.right = '10px';
+          messageElement.style.backgroundColor = '#ff0000';
+          messageElement.style.color = '#fff';
+          messageElement.style.padding = '10px';
+          messageElement.style.margin = '5px';
+          messageElement.style.borderRadius = '5px';
+          // Set timeout to hide the message after 30 seconds
+            setTimeout(() => {
+              messageElement.style.display = 'none';
+            }, 30000);
+        } else {
+          // Password not found in breach, set password inputs back to normal
+          passwordInputs.forEach((input) => {
+            input.removeAttribute('readonly');
+            input.removeAttribute('pattern');
+            input.removeAttribute('title');
+          });
+
+          // Hide the message element
+          messageElement.style.display = 'none';
+        }
+      })
+      .catch((error) => {
+        console.log('An error occurred:', error);
+      });
+  }, 500);
+}
+
+// Attach event listener to the email input field
+if (emailInput) {
+  emailInput.addEventListener('input', handleEmailChange);
+  emailInput.addEventListener('change', handleEmailChange);
+}
+});
+</script>
